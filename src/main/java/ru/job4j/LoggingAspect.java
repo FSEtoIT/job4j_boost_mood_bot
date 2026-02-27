fe5f0916558683d2409cc8debe1a97d786c0a4bf
FSE <amakeevmindraycom@yandex.ru>
2026-02-27 18:41:54 +0300

    DI, IoC, Beans, BeanFactory, ApplicationContext [#505289]

--- src/main/java/ru/job4j/LoggingAspect.java
+++ src/main/java/ru/job4j/LoggingAspect.java
@@ -1,4 +1,7 @@
 package ru.job4j;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class LoggingAspect {
 }
--- src/main/java/ru/job4j/RecommendationEngine.java
+++ src/main/java/ru/job4j/RecommendationEngine.java
@@ -1,4 +1,7 @@
 package ru.job4j;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RecommendationEngine {
 }

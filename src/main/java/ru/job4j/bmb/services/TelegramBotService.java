576a9bb5db78625e7c9c392ac37482b9b87737ff
FSE <amakeevmindraycom@yandex.ru>
2026-02-28 12:31:09 +0300

    Aware интерфейсы [#505301]

--- src/main/java/ru/job4j/bmb/services/TelegramBotService.java
+++ src/main/java/ru/job4j/bmb/services/TelegramBotService.java
@@ -2,12 +2,15 @@ package ru.job4j.bmb.services;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Service;
 import ru.job4j.bmb.model.Content;
 
 @Service
-public class TelegramBotService {
+public class TelegramBotService implements BeanNameAware {
+
     private final BotCommandHandler handler;
+    private String beanName;
 
     public TelegramBotService(BotCommandHandler handler) {
         this.handler = handler;
@@ -25,5 +28,11 @@ public class TelegramBotService {
     @PreDestroy
     public void destroy() {
         System.out.println("TelegramBotService уничтожается");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        this.beanName = name;
+        System.out.println("Имя бина в Spring-контексте: " + name);
     }
 }
